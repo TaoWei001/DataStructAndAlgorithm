5e019dfa40fdaeebd40ff5ab4eb077d6b3d171c9
TaoWei001 <1104148232@qq.com>
2021-09-25 10:22:21 +0800

    Java for queue

---
+++ 极客时间-数据结构与算法基础/queue.cpp
@@ -0,0 +1,43 @@
+/*
+ * @Author: TWWT
+ * @Date: 2021-09-25 09:39:19
+ * @LastEditTime: 2021-09-25 10:21:17
+ * @LastEditors: TWWT   
+ * @Description: In User Settings Edit
+ * @FilePath: \DataStructAndAlgorithm\极客时间-数据结构与算法基础\queue.cpp
+ */
+
+// 用数组实现的队列
+public class ArrayQueue {
+  // 数组：items，数组大小：n
+  private String[] items;
+  private int n = 0;
+  // head表示队头下标，tail表示队尾下标
+  private int head = 0;
+  private int tail = 0;
+
+  // 申请一个大小为capacity的数组
+  public ArrayQueue(int capacity) {
+    items = new String[capacity];
+    n = capacity;
+  }
+
+  // 入队
+  public boolean enqueue(String item) {
+    // 如果tail == n 表示队列已经满了
+    if (tail == n) return false;
+    items[tail] = item;
+    ++tail;
+    return true;
+  }
+
+  // 出队
+  public String dequeue() {
+    // 如果head == tail 表示队列为空
+    if (head == tail) return null;
+    // 为了让其他语言的同学看的更加明确，把--操作放到单独一行来写了
+    String ret = items[head];
+    ++head;
+    return ret;
+  }
+}
